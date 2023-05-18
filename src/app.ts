f328197afda2edf28cec8c41b172ab2136e817ee
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-18 01:50:45 -0300

    feat: crud memórias

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
-console.log('Hello Spacetime!')
+app.register(cors, {
+  origin: true,
+})
+
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on port http://localhost:3333')
+  })
